a0fab6098d9798ec705f2b879bb7ddb5b59f803c
amirrezajahanbakhsh <137860142+amirrezajahanbakhsh@users.noreply.github.com>
2023-07-01 19:33:09 +0330

    Add files via upload

---
+++ 1234.py
@@ -0,0 +1,13 @@
+x = int(input("adad ra vared konid"))
+
+z = x // 10
+
+print(z)
+
+z // 100
+
+print(z)
+
+z // 1000
+
+print(z)
---
+++ yekan.py
@@ -0,0 +1,14 @@
+a = float(input("adad ashari ra  vared konid"))
+z = int(input("adad yekan ra vared konid"))
+c = int(input("adad dahgan ra vared konid"))
+d = int(input("adad sadgan ra vared konid"))
+x = int(input("adad hezargan ra vared konid"))
+
+a / 10
+c * 10
+d * 100
+x * 1000
+
+sum = a + z + c + d + x 
+
+print(sum)
