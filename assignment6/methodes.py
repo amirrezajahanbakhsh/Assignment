5b796552a241a029b974aa8733a64cd8619167e7
amirrezajahanbakhsh <137860142+amirrezajahanbakhsh@users.noreply.github.com>
2023-07-11 13:20:34 +0330

    Add files via upload

---
+++ assignment6/methodes.py
@@ -0,0 +1,33 @@
+##COPY
+
+#cities = ['London', 'Paris','New York', 'Tehran', 'Mashhad' ]
+#cities_copy = cities.copy()
+#print("orginalmlist:", cities)
+#("copied cities", cities_copy)
+
+
+##INSERT
+
+
+#fruit = ['apple', 'banana', 'cherry', 'orange', 'mango']
+
+#fruit.insert(1, 'pineapple')
+#print(fruit)
+
+
+##INDEX
+
+
+#names = ['ali', 'mohamad', 'amirreza', 'arshia', 'kiya', ]
+
+#x = names.index('amirreza') 
+#print(x)
+
+##EXTEND
+
+
+countries = ['france', 'iran', 'japan', 'china']
+cities = ['paris', 'tehran', 'mashhad', 'tabriz']
+
+countries.extend(cities)
+print(countries)
