249e60a816bb24cfc284cc6dd02c9054ba00c05b
amirrezajahanbakhsh <137860142+amirrezajahanbakhsh@users.noreply.github.com>
2023-07-16 09:47:53 +0330

    Add files via upload

---
+++ assignment8/show_number.py
@@ -0,0 +1,13 @@
+number = int(input("enter your number: "))
+
+for i in range(number, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+
+
+for i in range(1, number + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
