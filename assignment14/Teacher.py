45a2f4068ebc54af4c80768fd96d989f0f6cee44
amirrezajahanbakhsh <137860142+amirrezajahanbakhsh@users.noreply.github.com>
2023-08-10 16:02:10 +0330

    Add files via upload

---
+++ assignment14/Teacher.py
@@ -0,0 +1,17 @@
+class Teacher:
+    firstname = None
+    age = None
+    height = None
+    Weight = None
+    lastname = None
+
+    def Write():
+        pass
+    def teaching():
+        pass
+    def taking_an_exam():
+        pass
+    def check_present_and_absent():
+        pass
+    def going_to_work():
+        pass
